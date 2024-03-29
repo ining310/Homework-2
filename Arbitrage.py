2f0b86bd86bd08a4f8eb1ee681623455b404eb25
ining310 <h94usu6@gmail.com>
2024-03-30 03:34:00 +0800

    update liquidity

--- Arbitrage.py
+++ Arbitrage.py
@@ -18,7 +18,7 @@ def getAmountOut(amountIn, reserveIn, reserveOut):
     if (reserveIn <= 0 or reserveOut <= 0):
         print("UniswapV2Library: INSUFFICIENT_LIQUIDITY")
         return 0
-    print("(997*",amountIn,"*",reserveOut,")/(1000*",reserveIn,"+997*",amountIn,") = ",(997*amountIn*reserveOut)/(1000*reserveIn+997*amountIn),sep="")
+    # print("(997*",amountIn,"*",reserveOut,")/(1000*",reserveIn,"+997*",amountIn,") = ",(997*amountIn*reserveOut)/(1000*reserveIn+997*amountIn),sep="")
     return (997*amountIn*reserveOut)/(1000*reserveIn+997*amountIn)
 
 def getAmountsOut(factory, amountIn, path):
@@ -28,8 +28,13 @@ def getAmountsOut(factory, amountIn, path):
     amounts = [amountIn]
     for i,factory in enumerate(path):
         if (i >= len(path)-1): break
+        if (path[i]==path[i+1]):
+            print("consecutive same token")
+            return []
         (reserveIn, reserveOut) = getReserves(factory,path[i],path[i+1])
-        amounts.append(getAmountOut(amounts[i],reserveIn,reserveOut))
+        amountOut = getAmountOut(amounts[i],reserveIn,reserveOut)
+        amounts.append(amountOut)
+        update(amountIn,amountOut,factory,path[i],path[i+1])
     return amounts
 
 def getReserves(factory, token0, token1):
@@ -39,6 +44,17 @@ def getReserves(factory, token0, token1):
         (reserve1, reserve0) = liquidity[(token1,token0)]
     return (reserve0, reserve1) if (factory == token0) else (reserve1, reserve0)
 
-path = ['tokenB','tokenD']
-amounts = getAmountsOut('tokenB',5,path)
+def update(amountIn,amountOut,factory,token0,token1):
+    tokenIn = token0 if (factory == token0) else token1
+    tokenOut = token1 if (factory == token0) else token0
+    if ((tokenIn,tokenOut) in liquidity):
+        (_reserveIn, _reserveOut) = liquidity[(tokenIn,tokenOut)]
+        liquidity[(tokenIn,tokenOut)] = (_reserveIn+amountIn, _reserveOut-amountOut)
+    else:
+        (_reserveOut, _reserveIn) = liquidity[(tokenOut,tokenIn)]
+        liquidity[(tokenOut,tokenIn)] = (_reserveOut-amountOut,_reserveIn+amountIn)
+
+# path = ['tokenB','tokenA','tokenB','tokenA','tokenB']
+# amounts = getAmountsOut('tokenB',5,path)
 print(amounts)
+tokens = ['tokenA','tokenB','tokenC','tokenD','tokenE']
