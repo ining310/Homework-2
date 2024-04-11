12fc53e806ff719fc47ac40135d3255cfdcb8fc9
ining310 <h94usu6@gmail.com>
2024-04-12 01:03:07 +0800

    del comment in .sol

--- hw2/test/Arbitrage.t.sol
+++ hw2/test/Arbitrage.t.sol
@@ -85,10 +85,6 @@ contract Arbitrage is Test {
         path[2] = address(tokenD);
         path[3] = address(tokenC);
         path[4] = address(tokenB);
-        // path[0] = address(tokenB);
-        // path[1] = address(tokenA);
-        // path[2] = address(tokenD);
-        // path[3] = address(tokenB);
         router.swapExactTokensForTokens(5 ether, 0 ether, path, arbitrager, block.timestamp+6000);
         /**
          * Please add your solution above
