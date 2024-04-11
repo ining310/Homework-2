4d26a1266650b09b61c4d273a2f08ac362bd72a4
ining310 <h94usu6@gmail.com>
2024-04-12 01:00:50 +0800

    del testing comment

--- Arbitrage.py
+++ Arbitrage.py
@@ -21,7 +21,6 @@ def getAmountOut(amountIn, reserveIn, reserveOut):
     if (reserveIn <= 0 or reserveOut <= 0):
         print("UniswapV2Library: INSUFFICIENT_LIQUIDITY")
         return 0
-    # print("(997*",amountIn,"*",reserveOut,")/(1000*",reserveIn,"+997*",amountIn,") = ",(997*amountIn*reserveOut)/(1000*reserveIn+997*amountIn),sep="")
     return (997*amountIn*reserveOut)/(1000*reserveIn+997*amountIn)
 
 def getAmountsOut(factory, amountIn, path):
@@ -31,13 +30,10 @@ def getAmountsOut(factory, amountIn, path):
     amounts = [amountIn]
     for i,factory in enumerate(path[:len(path)-1]):
         if (path[i]==path[i+1]):
-            # print("consecutive same token")
             return []
         (reserveIn, reserveOut) = getReserves(factory,path[i],path[i+1])
-        # print((reserveIn, reserveOut))
         amountOut = getAmountOut(amounts[i],reserveIn,reserveOut)
         amounts.append(amountOut)
-        # update(amountIn,amountOut,factory,path[i],path[i+1])
     return amounts
 
 def getReserves(factory, token0, token1):
@@ -57,31 +53,13 @@ def update(amountIn,amountOut,factory,token0,token1):
         (_reserveOut, _reserveIn) = liquidity[(tokenOut,tokenIn)]
         liquidity[(tokenOut,tokenIn)] = (_reserveOut-amountOut,_reserveIn+amountIn)
 
+# path: tokenB->tokenA->tokenD->tokenB, tokenB balance=10.930035
 def printFormat(path, amounts):
-    print(path[0],end="")
+    print("path:",path[0],end="")
     for _path in path[1:]:
         print("->",_path,end="",sep="")
     print(", ",path[-1]," balance=",amounts[-1],sep="")
 
 path = ['tokenB','tokenA','tokenD','tokenC','tokenB']
-# path = ['tokenB','tokenA','tokenD','tokenB']
 amounts = getAmountsOut('tokenB',5,path)
-# print('amounts:',amounts)
 printFormat(path,amounts)
-
-# tokens = ['tokenA','tokenB','tokenC','tokenD','tokenE']
-# tokens = ['tokenA','tokenC','tokenD','tokenE']
-# tokens = ['tokenC','tokenD','tokenE']
-# n = 4
-# # comb = combinations(tokens, n)
-# comb = permutations(tokens)
- 
-# for _comb in list(comb):
-#     # liquidity = liquidity_copied
-#     path = ['tokenB']+list(_comb)+['tokenB']
-#     print (path,getAmountsOut('tokenB',5,path))
-
-# '''
-# ['tokenB', 'tokenA', 'tokenE', 'tokenD', 'tokenC', 'tokenB'] [5, 5.655321988655322, 1.0583153138066885, 2.429786260142227, 5.038996197252911, 20.042339589188174]
-# ['tokenB', 'tokenA', 'tokenD', 'tokenC', 'tokenB'] [5, 5.655321988655322, 2.4587813170979333, 5.0889272933015155, 20.129888944077443]
-# '''
